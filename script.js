cb8f49d175037b1596c8fa36b225cca61ca2bc5d
Success Ikechukwu <ikechukwuidoko101@gmail.com>
2025-04-26 14:19:10 +0100

    changes 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function loadProducts() {
   try {
-    const response = await fetch("json-files/products.json");
+    const response = await fetch("./json-files/products.json");
     const data = await response.json();
     const MyProducts = data;
     // console.log(MyProducts[0]);
@@ -16,17 +16,34 @@ async function loadProducts() {
 loadProducts();
 
 function work(MyProducts) {
-  productList = [];
-  productCat = [];
+  const productList = [];
+  const productCat = [];
   MyProducts.forEach((product) => {
     productList.push(product);
     if (!productCat.includes(product.category)) {
-      productCat.push(product, categories);
+      productCat.push(product.category);
     }
   });
-  p(productCat);
+  const ProductsObject = classify();
+  function classify() {
+    const ProductsObject = {};
+    productCat.forEach((cat) => {
+      let catProducts = [];
+      MyProducts.forEach((product) => {
+        if (product.category == cat) {
+          catProducts.push(product);
+        }
+      });
+      ProductsObject[cat] = [catProducts];
+    });
+    return ProductsObject;
+  }
+  t(ProductsObject);
 }
 
 function p(e) {
   console.log(e);
+}
+function t(e) {
+  console.table(e);
 }
