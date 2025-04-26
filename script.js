ab615974a4b0c3134ef7e9c0eb0031ed269fdd2e
Success Ikechukwu <ikechukwuidoko101@gmail.com>
2025-04-26 12:49:24 +0100

    modified script

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-import data from "../json-files/emarket_products.json";
+async function loadProducts() {
+  try {
+    const response = await fetch("./json-files/emarket_products.json");
+    const data = await response.json();
+    const MyProducts = data;
+    console.table(MyProducts);
 
-console.log(data);
+    //
+
+    //
+  } catch (error) {
+    console.error("File MISSNG");
+  }
+}
+
+loadProducts();
